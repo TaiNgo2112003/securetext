74fcd32fc277898b263fbcf15b7c0f099367a72e
Ngo Ba Tan Tai <taingo2112003@gmail.com>
2025-04-26 19:17:39 +0700

    Mongodb config

--- Services/MongoDbService.cs
+++ Services/MongoDbService.cs
@@ -8,9 +8,9 @@ namespace SecureText.Services
 
         public MongoDbService()
         {
-            var connectionString = "mongodb+srv://taingo2112003:sKDQQdN3BnhXzcE7@securetext.7lxbomu.mongodb.net/?retryWrites=true&w=majority&appName=SecureText";
+            var connectionString = "mongodb+srv://taingo2112003:sKDQQdN3BnhXzcE7@securetext.7lxbomu.mongodb.net/?retryWrites=true&w=majority&tls=true&appName=SecureText";
             var client = new MongoClient(connectionString);
-            _database = client.GetDatabase("SecureTextDb"); // tên database bạn muốn (chưa có sẽ tự tạo)
+            _database = client.GetDatabase("SecureTextDb"); // tên database bạn muốn
         }
 
         public IMongoDatabase GetDatabase()
